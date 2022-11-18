5db6c87ed6036f2c5bdc2ab501db8647fcb7f878
Petar Iliev <petariliev2002@gmail.com>
2022-11-18 16:38:04 -0600

    Design UI

--- Image-Recognizer/ViewController.swift
+++ Image-Recognizer/ViewController.swift
@@ -6,14 +6,21 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func cameraPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    
 }
 
